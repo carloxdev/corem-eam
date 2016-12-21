7d59f872daabf19b3c4978b40dfe6c39a8636d18
Angelica <angelicagarciaalejo@gmail.com>
2016-12-21 13:44:21 -0600

    Js anexos TODO: all

--- activos/static/equipo/anexo/archivo.js
+++ activos/static/equipo/anexo/archivo.js
@@ -1,156 +1,45 @@
-/*-----------------------------------------------*\
-            GLOBAL VARS
-\*-----------------------------------------------*/
-
-var url_grid = window.location.origin + "/api/anexo/equipo/archivo/?equipo="
-var targeta_resultados = null
-
+var archivo = null;
 /*-----------------------------------------------*\
             LOAD
 \*-----------------------------------------------*/
 
-$(document).ready(function () {
+$(document).ready(function(){
+	
+	archivo = new Archivo()
+	archivo.eliminar()
 
-    targeta_resultados = new TargetaResultados()
+
 })
 
-/*-----------------------------------------------*\
-            OBJETO: RESULTADOS
-\*-----------------------------------------------*/
+function Archivo(){
 
-function TargetaResultados() {
-
-    this.grid = new GridPrincipal()
+	
 }
-
-/*-----------------------------------------------*\
-            OBJETO: GRID
-\*-----------------------------------------------*/
-
-function GridPrincipal() {
-
-    this.$id = $("#grid_principal")
-    this.kfuente_datos = null
-    this.kgrid = null
-
-    this.init()
-}
-GridPrincipal.prototype.init = function () {
-    kendo.culture("es-MX")
-
-    this.kfuente_datos = new kendo.data.DataSource(this.get_FuenteDatosConfig())
-
-    this.kgrid = this.$id.kendoGrid({
-        dataSource: this.kfuente_datos,
-        columnMenu: false,
-        groupable: false,
-        sortable: false,
-        editable: false,
-        resizable: true,
-        selectable: true,
-        scrollable: false,
-        columns: this.get_Columnas(),
-        scrollable: true,
-        noRecords: {
-            template: "<div class='grid-empy'> Sin Registros </div>"
-        },
-    })
-
-    this.kgrid.data("kendoGrid").resize()
-}
-GridPrincipal.prototype.get_Campos = function (e) {
- return {
-       
-    }
-
-}
-GridPrincipal.prototype.get_Columnas = function (e) {
-
-    return [      
-        { field: "equipo" , title: "Equipo", width: "120px"},
-        { field: "descripcion" , title: "Descripción", width: "120px"},
-        {
-           command: [
-               {
-                   text: "Editar",
-                   click: this.click_BotonEditar,
-                   className: "boton_editar"
-               },
-               {
-                    text: "Eliminar",
-                    click: this.click_BotonEliminar,
-                    className: "boton_eliminar"
-               },
-               {
-                    text: "Descargar",
-                    click: this.click_BotonDescargar,
-                    className: "boton_descargar"
-               }
-           ],
-           title: " ",
-           width: "120px"
-        },
-    ]
-}
-GridPrincipal.prototype.get_FuenteDatosConfig = function (e) {
-    var id_equipo = $('#id_equipo').val()
-
-    return {
-
-        serverPaging: true,
-        pageSize: 30,
-        transport: {
-            read: {
-
-                url: url_grid+id_equipo,
-                type: "GET",
-                dataType: "json",
-            },
-            parameterMap: function (data, action) {
-                if (action === "read") {
-
-                    return {
-                        page: data.page,
-                        pageSize: data.pageSize,
-                        
-                    }
-                }
-            }
-        },
-        schema: {
-            data: "results",
-            total: "count",
-            model: {
-                fields: this.get_Campos()
-            }
-        },
-        error: function (e) {
-            alert("Valio gaver")
-        },
-    }
-}
-GridPrincipal.prototype.buscar =  function() {
-  this.kfuente_datos.page(1)   
-}
-GridPrincipal.prototype.click_BotonEditar = function (e) {
-
-    e.preventDefault()
-    var fila = this.dataItem($(e.currentTarget).closest('tr'))
-    console.log(fila);
-    window.location.href = url_editar + fila.pk;
-    e.preventDefault()
-    var fila = this.dataItem($(e.currentTarget).closest('tr'))
-    window.location.href = url_editar + fila.pk;
-}
-GridPrincipal.prototype.click_BotonEliminar = function (e) {
-
-    e.preventDefault()
-}
-GridPrincipal.prototype.click_BotonDescargar = function (e) {
-
-    e.preventDefault()
-    var fila = this.dataItem($(e.currentTarget).closest('tr'))
-    var archivo = fila.archivo
-    //alert(archivo)
-
+Archivo.prototype.eliminar = function(){
+	$('.eliminar').on('click', function(){
+		id_anexo = $(this).attr('id');
+		console.log(id_anexo);
+		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+		$('#id_anexo').val(id_anexo);
+		$('#modal_eliminar').modal('show');
+		$('#boton_eliminar').on('click', function(){
+			$.ajax({
+			url: '/api/anexosarchivo/'+id_anexo,
+			headers: { "X-CSRFToken": csrftoken },
+			method: "DELETE",
+			success: function (){
+				console.log("exito");
+				$('#modal_eliminar').modal('hide');
+				location.reload();
+			},
+			error: function(e){
+				alert(e);
+				$('#modal_eliminar').modal('hide')
+			}
+           
+                    
+        	});
+		});
+		
+	}); 
 }
--- activos/static/equipo/anexo/texto.js
+++ activos/static/equipo/anexo/texto.js
@@ -1,135 +1,53 @@
-/*-----------------------------------------------*\
-            GLOBAL VARS
-\*-----------------------------------------------*/
-
-var url_grid = window.location.origin + "/api/anexo/equipo/texto/?equipo="
-var targeta_resultados = null
-
+var archivo = null;
 /*-----------------------------------------------*\
             LOAD
 \*-----------------------------------------------*/
 
-$(document).ready(function () {
+$(document).ready(function(){
+	
+	texto = new Texto()
+	texto.eliminar()
+	texto.init()
 
-    targeta_resultados = new TargetaResultados()
+
 })
 
-/*-----------------------------------------------*\
-            OBJETO: RESULTADOS
-\*-----------------------------------------------*/
+function Texto(){
 
-function TargetaResultados() {
+	this.$textarea = $('#id_texto')
 
-    this.grid = new GridPrincipal()
 }
 
+Texto.prototype.init = function (){
 
-/*-----------------------------------------------*\
-            OBJETO: GRID
-\*-----------------------------------------------*/
+	this.$textarea.wysihtml5();
 
-function GridPrincipal() {
-
-    this.$id = $("#grid_principal")
-    this.kfuente_datos = null
-    this.kgrid = null
-
-    this.init()
 }
-GridPrincipal.prototype.init = function () {
-    kendo.culture("es-MX")
-
-    this.kfuente_datos = new kendo.data.DataSource(this.get_FuenteDatosConfig())
-
-    this.kgrid = this.$id.kendoGrid({
-        dataSource: this.kfuente_datos,
-        columnMenu: false,
-        groupable: false,
-        sortable: false,
-        editable: false,
-        resizable: true,
-        selectable: true,
-        scrollable: false,
-        columns: this.get_Columnas(),
-        scrollable: true,
-        noRecords: {
-            template: "<div class='grid-empy'> Sin Registros </div>"
-        },
-    })
-
-    this.kgrid.data("kendoGrid").resize()
-}
-GridPrincipal.prototype.get_Campos = function (e) {
-
-    return {
-        clave: { type: "string"},
-        descripcion: { type: "string" },
-    }
-}
-GridPrincipal.prototype.get_Columnas = function (e) {
-
-    return [      
-        { field: "equipo" , title: "Equipo", width: "120px" },
-        { field: "texto" , title: "Texto", width: "200px" },
-        {
-           command: [
-                 {  
-                    text: "Editar",
-                    click: this.click_BotonEditar,
-                    className: "boton_editar"
-                },
-                {
-                    text: "Eliminar",
-                    click: this.click_BotonEliminar,
-                    className: "boton_eliminar"
-                },
-           ],
-           title: " ",
-           width: "90px"
-        },
-    ]
-}
-GridPrincipal.prototype.get_FuenteDatosConfig = function (e) {
-    
-    var id_equipo = $('#id_equipo').val()
-
-    return {
-
-        serverPaging: true,
-        pageSize: 30,
-        transport: {
-            read: {
-
-                url: url_grid+id_equipo,
-                type: "GET",
-                dataType: "json",
-            },
-            parameterMap: function (data, action) {
-                if (action === "read") {
-
-                    return {
-                        page: data.page,
-                        pageSize: data.pageSize,
-                        
-                    }
-                }
-            }
-        },
-        schema: {
-            data: "results",
-            total: "count",
-            model: {
-                fields: this.get_Campos()
-            }
-        },
-        error: function (e) {
-            alert("Valio gaver")
-        },
-    }
-}
-GridPrincipal.prototype.click_BotonEditar = function (e) {
-
-    e.preventDefault()
-    // TODO: Crear vista en el servidor para edición de anexos
-
+Texto.prototype.eliminar = function(){
+	$('.eliminar').on('click', function(){
+		id_anexo = $(this).attr('id');
+		console.log(id_anexo);
+		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+		$('#id_anexo').val(id_anexo);
+		$('#modal_eliminar').modal('show');
+		$('#boton_eliminar').on('click', function(){
+			$.ajax({
+			url: '/api/anexostexto/'+id_anexo,
+			headers: { "X-CSRFToken": csrftoken },
+			method: "DELETE",
+			success: function (){
+				console.log("exito");
+				$('#modal_eliminar').modal('hide');
+				location.reload();
+			},
+			error: function(e){
+				alert(e);
+				$('#modal_eliminar').modal('hide')
+			}
+           
+                    
+        	});
+		});
+		
+	}); 
 }
