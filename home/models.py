a8c88c9c2614df2e8f01be59ddfbf49134b8bec8
Angelica <angelicagarciaalejo@gmail.com>
2016-12-22 11:36:23 -0600

    Mejora, se organiza el directorio donde se guardan los archivos de anexos

--- home/models.py
+++ home/models.py
@@ -6,6 +6,8 @@ from django.db import models
 # from django.core.exceptions import ValidationError
 
 from .validators import validate_image
+from .utilities import get_FilePath
+from .utilities import get_ImagePath
 # from .validators import valid_extension
 # Otros modelos
 
@@ -14,7 +16,7 @@ from activos.models import Equipo
 
 class AnexoArchivo(models.Model):
     equipo = models.ForeignKey(Equipo)
-    archivo = models.FileField(upload_to='equipos/files')
+    archivo = models.FileField(upload_to=get_FilePath)
     descripcion = models.CharField(max_length=255, null=True, blank=True)
 
 
@@ -27,6 +29,6 @@ class AnexoTexto(models.Model):
 class AnexoImagen(models.Model):
 
     equipo = models.ForeignKey(Equipo)
-    ruta = models.ImageField(upload_to='equipos/img',
+    ruta = models.ImageField(upload_to=get_ImagePath,
                              validators=[validate_image])
     descripcion = models.CharField(max_length=50, null=True)
