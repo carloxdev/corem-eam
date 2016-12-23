dc4db6cf8ab5f2a2034db77a35ba2ac99bc6934c
Angelica <angelicagarciaalejo@gmail.com>
2016-12-23 12:42:53 -0600

    Modificando funcion para generar el path dinamico para los anexos

--- home/utilities.py
+++ home/utilities.py
@@ -4,14 +4,24 @@ import os
 
 
 def get_ImagePath(instance, filename):
-    upload_dir = os.path.join('equipos', 'anexos', 'img')
+    if (instance.equipo_id):
+        upload_dir = os.path.join(
+            'equipos', instance.equipo_id, 'anexos', 'img')
+    elif (instance.articulo_id):
+        upload_dir = os.path.join(
+            'articulos', instance.articulo_id, 'anexos', 'img')
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
     return os.path.join(upload_dir, filename)
 
 
 def get_FilePath(instance, filename):
-    upload_dir = os.path.join('equipos', 'anexos', 'files')
+    if (instance.equipo_id):
+        upload_dir = os.path.join(
+            'equipos', instance.equipo_id, 'anexos', 'files')
+    elif (instance.articulo_id):
+        upload_dir = os.path.join(
+            'articulos', instance.articulo_id, 'anexos', 'files')
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
     return os.path.join(upload_dir, filename)
